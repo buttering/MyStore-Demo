f2317d873b058f2bfb4bb93284a56c39152356bd
buttering <384123539@qq.com>
2023-05-12 14:28:42 +0800

    添加用户密码加密逻辑和时间日期格式化逻辑
    存在record与mybtis-plus不兼容的情况待解决

---
+++ src/main/java/com/csu/mypetstore/api/dto/RegisterUserDTO.java
@@ -0,0 +1,4 @@
+package com.csu.mypetstore.api.dto;
+
+public class RegisterUserDTO {
+}
--- src/main/java/com/csu/mypetstore/api/service/impl/UserServiceImpl.java
+++ src/main/java/com/csu/mypetstore/api/service/impl/UserServiceImpl.java
@@ -4,7 +4,6 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.csu.mypetstore.api.common.CONSTANT;
 import com.csu.mypetstore.api.common.CommonResponse;
 import com.csu.mypetstore.api.domain.User;
-import com.csu.mypetstore.api.domain.User_class;
 import com.csu.mypetstore.api.persistence.UserMapper;
 import com.csu.mypetstore.api.service.UserService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,14 +18,15 @@ public class UserServiceImpl implements UserService {
     private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
 
-    private final static HashMap<String, String> fieldMap = new HashMap<>();
+    private final static HashMap<String, String> FIELD_MAP = new HashMap<>();
 
     public UserServiceImpl(UserMapper userMapper, PasswordEncoder passwordEncoder) {
         this.userMapper = userMapper;
         this.passwordEncoder = passwordEncoder;
-        fieldMap.put("username", "用户名");
-        fieldMap.put("phone", "电话号码");
-        fieldMap.put("email", "电子邮件");
+
+        FIELD_MAP.put("username", "用户名");
+        FIELD_MAP.put("phone", "电话号码");
+        FIELD_MAP.put("email", "电子邮件");
     }
 
     @Override
@@ -40,7 +40,7 @@ public class UserServiceImpl implements UserService {
 
         boolean isPasswordMatch = passwordEncoder.matches(password, user.password());
         if (isPasswordMatch) {
-            user = user.withPassword(StringUtils.EMPTY);
+            user = user.withPassword(StringUtils.EMPTY);  // 和字符串处理相关的操作（判等、判空、null等），使用工具类
             return CommonResponse.createResponseForSuccess(user);
         } else {
             return CommonResponse.createResponseForError("用户名或密码错误");
@@ -50,7 +50,7 @@ public class UserServiceImpl implements UserService {
     // 检查字段是否重复
     @Override
     public CommonResponse<Object> checkFieldDuplication(String fieldName, String fieldValue) {
-        String chineseFieldName = fieldMap.get(fieldName);
+        String chineseFieldName = FIELD_MAP.get(fieldName);
         if (chineseFieldName == null){
             return CommonResponse.createResponseForError("参数错误");
         }
@@ -65,26 +65,6 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-//    public CommonResponse<Object> register(User user) {
-//        CommonResponse<Object> checkResult = checkFieldDuplication("username", user.getUsername());
-//        if (!checkResult.isSuccess()) return checkResult;
-//        checkResult = checkFieldDuplication("email", user.getEmail());
-//        if (!checkResult.isSuccess()) return checkResult;
-//        checkResult = checkFieldDuplication("phone", user.getPhone());
-//        if (!checkResult.isSuccess()) return checkResult;
-//
-//        // TODO: 加密
-//        String password = "9ABB136AA0B984E8";
-//
-//        user.setCreateTime(LocalDateTime.now());
-//        user.setUpdateTime(LocalDateTime.now());
-//        int rows = userMapper.insert(user);
-//        if (rows == 0) {
-//            return CommonResponse.createResponseForError("注册用户失败");
-//        }
-//        return CommonResponse.createResponseForSuccess("注册用户成功");
-//    }
-
     public CommonResponse<Object> register(User user) {
         CommonResponse<Object> checkResult = checkFieldDuplication("username", user.username());
         if (!checkResult.isSuccess()) return checkResult;
